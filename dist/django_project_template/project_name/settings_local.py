e8c75faea568d917dccf9110ff16f3d80217ff0b
Ionel Maries Cristian <contact@ionelmc.ro>
2012-10-24 02:30:13 +0300

    Minor tweaks/updates.

--- dist/django_project_template/project_name/settings_local.py
+++ dist/django_project_template/project_name/settings_local.py
@@ -1,8 +1,8 @@
 from .settings import *
 
-# don't repeat connection OPTIONS here, the database server behavior needs to be the same in 
+# don't repeat connection OPTIONS here, the database server behavior needs to be the same in
 # development
-DATABASES['default']['NAME'] = '{{ project_name }}_prod'
+DATABASES['default']['NAME'] = '{{ project_name }}'
 
 INSTALLED_APPS += (
     'debug_toolbar',
