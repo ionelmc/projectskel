8720e6d4fa3217a2a56257a75e1055c4166831ef
Ionel Maries Cristian <contact@ionelmc.ro>
2012-10-26 23:59:35 +0300

    Update the local settings template to have some sensible defaults for debug_toolbar.

--- dist/django_project_template/project_name/settings_local.py
+++ dist/django_project_template/project_name/settings_local.py
@@ -11,3 +11,10 @@ INSTALLED_APPS += (
 MIDDLEWARE_CLASSES = (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ) + MIDDLEWARE_CLASSES
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True,
+    'HIDE_DJANGO_SQL': False,
+    'ENABLE_STACKTRACES' : True,
+}
